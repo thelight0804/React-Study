715d7457be6ba5e829ecf0f5a1e5a0e710e0ae91
thelight0804 <thelight0804@gmail.com>
2022-10-19 15:33:44 +0900

    feat: Add props for modal movie title
    - Modal 창을 각 제목에 맞게 출력하기

--- React basic/src/App.js
+++ React basic/src/App.js
@@ -5,6 +5,7 @@ function App() {
 
   let [nextMovie, setNextMovie] = useState(['Ant-Man and the Wasp', 'Guardians of the Galaxy', 'The Marvels']);
   let [movieSub, setMovieSub] = useState(['Quantumania', 'Volume 3', '']);
+  let [movieIndex, setMovieIndex] = useState(0);
   let [star, setStar] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
@@ -17,6 +18,7 @@ function App() {
           return (
             <div className="list">
               <h4 onClick={ () => {
+                setMovieIndex(i)
                 modal ? setModal(false) : setModal(true)
               }}>{nextMovie[i]} </h4>
               <p>{movieSub[i]}
@@ -24,11 +26,11 @@ function App() {
                 let copy = [...star];
                 copy[i] += 1;
                 setStar(copy)} }>⭐</span> {star[i]} </p>
-              { modal ? <Modal nextMovie={nextMovie} color={'#ffccff'} setNextMovie={setNextMovie}/> : null }
           </div>
           )
         })
       }
+      { modal ? <Modal nextMovie={nextMovie} movieIndex = {movieIndex} color={'#dbaaaa'} setNextMovie={setNextMovie}/> : null }
     </div> //"App" End
   ); //App End
 }
@@ -36,14 +38,10 @@ function App() {
 function Modal(props){
   return(
       <div className="modal" style={{background : props.color}}>
-        <h4>{props.nextMovie[0]}</h4>
+        <h4>{props.nextMovie[props.movieIndex]}</h4>
         <p>Date</p>
         <p>exp</p>
-        <button onClick={()=>{
-          let temp = [...props.nextMovie];
-          temp[0] = "Ironman";
-          props.setNextMovie(temp)
-        }}>Edit</button>
+        <button>Edit</button>
       </div>
   )
 }
