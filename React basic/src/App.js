825f818911a3d1500aa8b637fc77890286c607d8
thelight0804 <thelight0804@gmail.com>
2022-10-29 14:07:43 +0900

    feat: Add title input and delete post button
    - input 1 : 사용자가 입력한 글 다루기

--- React basic/src/App.js
+++ React basic/src/App.js
@@ -8,6 +8,7 @@ function App() {
   let [movieIndex, setMovieIndex] = useState(0);
   let [star, setStar] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [exp, setExp] = useState('');
 
   return (
     <div className="App">
@@ -22,15 +23,35 @@ function App() {
                 modal ? setModal(false) : setModal(true)
               }}>{nextMovie[i]} </h4>
               <p>{movieSub[i]}
-              <span onClick={ ()=>{
+
+              <span onClick={ (e)=>{
+                e.stopPropagation();
                 let copy = [...star];
                 copy[i] += 1;
                 setStar(copy)} }>⭐</span> {star[i]} </p>
+              <button onClick={()=>{
+                let copy = [...nextMovie];
+                copy.splice(i,1);
+                setNextMovie(copy);
+                copy = [...movieSub];
+                copy.splice(i, 1);
+                setMovieSub(copy);
+              }}>
+              Delete</button>
           </div>
           )
         })
       }
       { modal ? <Modal nextMovie={nextMovie} movieIndex = {movieIndex} color={'#dbaaaa'} setNextMovie={setNextMovie}/> : null }
+      <input onChange={(e)=>{
+        setExp(e.target.value);
+        console.log(exp);
+      }}></input>
+      <button onClick={()=>{
+        let copy = [...nextMovie];
+        copy.unshift(exp);
+        setNextMovie(copy);
+      }}>Enter</button>
     </div> //"App" End
   ); //App End
 }
