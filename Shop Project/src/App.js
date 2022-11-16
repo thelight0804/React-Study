ecaadc56e85a8e8df339449fd91e18287def1ebb
thelight0804 <thelight0804@gmail.com>
2022-11-16 17:48:25 +0900

    perf: Use component, map for productList
    - src의 image를 public으로 이동했습니다
    - productList.js : 상품 리스트 컴포넌트
    - App.js : productList를 불러화 컴포넌트로 만들었습니다

--- Shop Project/src/App.js
+++ Shop Project/src/App.js
@@ -7,10 +7,7 @@ import './App.css';
 import { useState } from 'react';
 import {Container, Nav, Navbar, Row, Col} from 'react-bootstrap';
 
-import col1 from './img/Row_Galaxy-Z-Flip4.png';
-import col2 from './img/Row_Galaxy-Z-Fold4.png';
-
-// import a from "./data.js";
+import ProductList from "./productList";
 import {product} from "./data.js";
 
 
@@ -32,30 +29,9 @@ function App() {
           </Container>
         </Navbar>
       </div>
-     {/* <div className = "main-bg" style={{backgroundImage : 'url('+main_bg+')'}}> </div> */}
-     <div className = "main-bg"> </div>
-     {/* <div className = "flipper-middle"></div> */}
-      <Row>
-        <Col sm>
-          <img src={col1} width="80%"/>
-          <h4>{cellphones[0].title}</h4>
-          <p>{cellphones[0].content}</p>
-          <h5>{cellphones[0].price}</h5>
-        </Col>
-        <Col sm>
-          {/* <div className = "flipper-middle"></div> */}
-          <img src={col2} width="80%"/>
-          <h4>{cellphones[1].title}</h4>
-          <p>{cellphones[1].content}</p>
-          <h5>{cellphones[1].price}</h5>
-        </Col>
-        <Col sm>
-          <img src={process.env.PUBLIC_URL + "/img/Row_Galaxy-S22.png"} width="80%"/>
-          <h4>{cellphones[2].title}</h4>
-          <p>{cellphones[2].content}</p>
-          <h5>{cellphones[2].price}</h5>
-        </Col>
-      </Row>
+      
+      <div className='main-bg'></div>
+        <ProductList/>
      </div>
   );
 }
---
+++ Shop Project/src/productList.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import {Row, Col} from 'react-bootstrap';
+
+import {product} from "./data.js";
+
+function Product(){
+  let [cellphones] = useState(product);
+  let [imageURL] = useState(["/img/Row_Galaxy-Z-Flip4.png", "/img/Row_Galaxy-Z-Fold4.png", "/img/Row_Galaxy-S22.png"])
+  return(
+    <>
+      <Row>
+        {cellphones.map(function(a, i){
+          return (
+            <Col sm>
+              <img src={process.env.PUBLIC_URL + imageURL[i]} width="80%"/>
+              <h4>{cellphones[i].title}</h4>
+              <p>{cellphones[i].content}</p>
+              <h5>{cellphones[i].price}</h5>
+            </Col>
+        )})}
+      </Row>
+   </>
+  )
+}
+
+export default Product;
