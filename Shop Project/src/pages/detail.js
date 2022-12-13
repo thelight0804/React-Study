01607ccd231e4163a59c1a3731205ac60e373353
thelight0804 <thelight0804@gmail.com>
2022-12-13 17:06:55 +0900

    Add input amount that alert not a number
    - Lifecycle과 useEffect 2

--- Shop Project/src/pages/detail.js
+++ Shop Project/src/pages/detail.js
@@ -4,17 +4,22 @@ import { useParams } from 'react-router-dom';
 
 function Detail(props){
 
-  useEffect(()=>{
-    setTimeout(()=>{setAlert(false)}, 2000);
-  })
-
-  
-
   let {id} = useParams();
   let dataId = props.cellphones.find(function(x){
     return x.id == id;
   })
   let [alert, setAlert] = useState(true);
+  let [amount, setAmount] = useState(0);
+  let [numAlert, setNumAlert] = useState(true);
+
+  useEffect(()=>{
+    let timer = setTimeout(()=>{setAlert(false)}, 2000);
+    isNaN(amount) ? setNumAlert(false) : setNumAlert(true);
+    return() => {
+      clearTimeout(timer);
+    }
+    }, [amount])
+  
   return (
     <>
       <div className="container">
@@ -30,6 +35,13 @@ function Detail(props){
             <h4 className="pt-5">{dataId.title}</h4>
             <p>{dataId.content}</p>
             <p>{dataId.price}\</p>
+            <div>
+              {numAlert ? null : <NumAlert/>}
+              <p style={{display:'inline'}}>수량 </p>
+              <input type="text" placeholder={amount} onChange={(e)=>{
+                setAmount(e.target.value);
+              }}/>
+            </div>
             <button className="btn btn-danger">주문하기</button>
           </div>
         </div>
@@ -41,7 +53,15 @@ function Detail(props){
 function Sale(){
   return(
     <div className='alert alert-warning'>
-    2초이내 구매시 할인!
+      2초이내 구매시 할인!
+    </div>
+  )
+}
+
+function NumAlert(){
+  return(
+    <div className='alert alert-warning'>
+      숫자만 입력이 가능합니다.
     </div>
   )
 }
