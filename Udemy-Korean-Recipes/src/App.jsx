08b09c20d278f648e920a6fab38c7ec3bee61e1a
thelight0804 <thelight0804@gmail.com>
2023-10-02 18:04:21 +0900

    Add css style use css module
    css module을 사용해서 css 스타일 적용

--- Udemy-Korean-Recipes/src/App.jsx
+++ Udemy-Korean-Recipes/src/App.jsx
@@ -2,9 +2,12 @@ import './App.css'
 import Recipe from './components/Recipe'
 
 function App() {
-    return (
-        <Recipe />
-    )
+    return <main>
+        <Recipe menu={'참치 비빔밥'} body={'고추참치 고추참치'}/>
+        <Recipe menu={'야채 비빔밥'} body={'아삭한 상추'}/>
+        <Recipe menu={'초콜릿 비빔밥'} body={'고추장 대신 누텔라'}/>
+        <Recipe menu={'고기 비빔밥'} body={'돼지고기 보다는 소고기'}/>
+    </main>
 }
 
 export default App
--- Udemy-Korean-Recipes/src/components/Recipe.jsx
+++ Udemy-Korean-Recipes/src/components/Recipe.jsx
@@ -1,12 +1,13 @@
+import styles from './Recipe.module.css';
+
 const menu = ['참치 비빔밥', '야채 비빔밥']
 
-function Recipe() {
-    const randomMenu = Math.random() > 0.5 ? menu[0] : menu[1];
+function Recipe(props) {
 
     return (
-        <div>
-            <p>{randomMenu}</p>
-            <p>냉장고를 열어봐라</p>
+        <div className={styles.recipe}>
+            <p>메뉴 : {props.menu}</p>
+            <p>{props.body}</p>
         </div>
     );
 }
