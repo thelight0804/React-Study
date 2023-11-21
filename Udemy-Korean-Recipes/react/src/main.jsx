4a650b916272c07d5bef5a7efb258994a1af4ef1
thelight0804 <thelight0804@gmail.com>
2023-11-21 14:11:40 +0900

    Study: react router

--- Udemy-Korean-Recipes/react/src/main.jsx
+++ Udemy-Korean-Recipes/react/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import NewRecipe from './components/NewRecipe.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-recipe', element: <NewRecipe /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
